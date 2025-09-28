e3e9e8697909b5cef808132530ee111294d6c156
SY <shiiyan79@gmail.com>
2025-09-28 23:28:10 +0900

    Squashed commit of the following:
    commit a310c60419dd07e8b26bdcd25f3d72c5a26adc1a
    Author: SY <shiiyan79@gmail.com>
    Date:   Sun Sep 28 23:27:50 2025 +0900
    
        complete step2

--- assembly/skeleton/step2/asm_arm64.s
+++ assembly/skeleton/step2/asm_arm64.s
@@ -7,18 +7,16 @@
 
 // func Add(a, b int64) int64
 TEXT ·Add(SB), $0-24
-	// TODO: 引数 a を R0 レジスタに読み込む
-
-	// TODO: 引数 b を R1 レジスタに読み込む
-
-	// TODO: R0 と R1 を加算（結果は R0 に格納される）
-
-	// TODO: 結果を戻り値の位置に書き込む
-
+	MOVD a+0(FP), R0 // 引数 a を R0 レジスタに読み込む
+	MOVD b+8(FP), R1 // 引数 b を R1 レジスタに読み込む
+	ADD R1, R0, R0 // R0 と R1 を加算（結果は R0 に格納される
+	MOVD R0, ret+16(FP) // 結果を戻り値の位置に書き込む
 	RET
 
 // func Sub(a, b int64) int64
 TEXT ·Sub(SB), $0-24
-	// TODO: 引き算を実装
-
+	MOVD a+0(FP), R0 
+	MOVD b+8(FP), R1 
+	SUB R1, R0, R0 
+	MOVD R0, ret+16(FP) 
 	RET
